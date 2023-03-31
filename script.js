823bb9c9af2d04c7e08e81cac651095442f681fe
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-31 18:36:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // your code here
+var url = document.querySelector("h3").innerHTML;
+var btn = document.getElementById("button");
+btn.addEventListener('click',fetchData);
+
+function fetchData(){
+	var nameData = document.getElementById("name");
+	var nameAttr = nameData.getAttribute("name");
+	var yearData = document.getElementById("year");
+	var yearAttr = yearData.getAttribute("name");
+	var str = "";
+	if(nameData.value && yearData){
+		str = url + `?${nameAttr}=${nameData.value}&${yearAttr}=${yearData.value}`;
+		document.querySelector("h3").innerHTML = str;
+	}
+	else if(nameData && !yearData){
+		str = url + `?${nameAttr}=${nameData.value}`;
+		document.querySelector("h3").innerHTML = str;
+	}
+	
+	
+	// alert(url);
+	
+	
+}
+
